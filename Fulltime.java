9bd789e524a9fefc99948120618a963e4953afd8
Ann Karagwa <annkaragwa@gmail.com>
2024-04-29 00:26:11 +0300

    initialized the instance variables in the default constructor

--- Fulltime.java
+++ Fulltime.java
@@ -1,46 +1,41 @@
 import java.util.Scanner;
 
-public class Fulltime extends Employee{
-    public double monthlyBasicSalary;
-    public double taxDeduction;
+public class Fulltime extends Employee {
+    private double monthlyBasicSalary;
+    private double taxDeduction;
+    private double netPay;
 
-    public double netPay;
-
-    public void getNetPay(){
+    public Fulltime() {
+        Scanner keyboard = new Scanner(System.in);
         System.out.println("Enter the monthly salary of the fulltime employee:");
-        Scanner keyboard= new Scanner (System.in);
-        monthlyBasicSalary= keyboard.nextDouble();
+        monthlyBasicSalary = keyboard.nextDouble();
         System.out.println("Enter the tax of the fulltime employee:");
-        taxDeduction= keyboard.nextDouble();
-        netPay=monthlyBasicSalary-taxDeduction;
+        taxDeduction = keyboard.nextDouble();
+        netPay = monthlyBasicSalary - taxDeduction;
         keyboard.close();
-
-    }
-    public void printStaffDetails(){
-        System.out.println("StaffNo: "+StaffNo);
-        System.out.println("StaffName: "+StaffName);
-        System.out.println("StaffAge: "+StaffAge);
-        System.out.println("StaffSalary: "+netPay);
     }
 
-    public static void main(String[] args){
-        Scanner keyboard= new Scanner(System.in);
+    public void printStaffDetails() {
+        System.out.println("StaffNo: " + StaffNo);
+        System.out.println("StaffName: " + StaffName);
+        System.out.println("StaffAge: " + StaffAge);
+        System.out.println("StaffSalary: " + netPay);
+    }
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
         System.out.println("Welcome to J and J enterprises.");
         System.out.println("Enter the number of fulltime employees: ");
-        int numberOfEmployees=keyboard.nextInt();
-        Fulltime [] employee= new Fulltime[numberOfEmployees];
-       
-        for (int i=0; i<employee.length; i++){
-            employee[i] =new Fulltime();
-            employee[i].enterDetails();
-            employee[i].getNetPay();
+        int numberOfEmployees = keyboard.nextInt();
+        Fulltime[] employees = new Fulltime[numberOfEmployees];
 
+        for (int i = 0; i < employees.length; i++) {
+            employees[i] = new Fulltime();
         }
 
-        for(Fulltime f: employee){
-            f.printStaffDetails();
+        for (Fulltime fulltimeEmployee : employees) {
+            fulltimeEmployee.printStaffDetails();
         }
         keyboard.close();
-        
     }
 }
